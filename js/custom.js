20a6dd3bd8ec890ff5119e9c21599617d48c0ae1
arara90 <ararajo@gmail.com>
2021-03-16 00:26:31 +0900

    210315

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,19 @@
-const projectBtn = document.getElementById("work1");
+
+//elements
 const customModal = document.querySelector(".custom-modal");
 const closeBtn = document.querySelector(".modal-close-button")
+// const projectBtn1 = document.getElementById("work1");
+// const projectBtn2 = document.getElementById("work2");
+// const projectBtn3 = document.getElementById("work3");
+const projectBtn4 = document.getElementById("work4");
 
-const openModal = () =>{
-    customModal.classList.remove('hidden');
-}
+//modal
+const openModal=()=>customModal.classList.remove('hidden');
+const closeModal=()=>customModal.classList.add('hidden');
 
-const closeModal = () =>{
-    customModal.classList.add('hidden');
-}
-
-projectBtn.addEventListener("click", openModal);
+//EventListener
 closeBtn.addEventListener("click", closeModal);
-
-
-
-
-//filedownload
-const zalesiaDownload = document.getElementById("zalesia");
-const downloadZal = () => {
-    alert('heelo')
-    // window.location.assign('https://github.com/arara90/images/raw/master/Lumos/findingway.png?raw=true');
-}
-zalesiaDownload.addEventListener("click", downloadZal);
+// projectBtn1.addEventListener("click", openModal);
+// projectBtn2.addEventListener("click", openModal);
+// projectBtn3.addEventListener("click", openModal);
+projectBtn4.addEventListener("click", openModal);
