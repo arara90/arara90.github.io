f5e9473191bfbea082ad7098c307c3339464609c
arara90 <ararajo@gmail.com>
2021-03-15 22:40:01 +0900

    project modal ing

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,22 @@
-const projectBtn = document.getElementById("work1");
+//elements
 const customModal = document.querySelector(".custom-modal");
 const closeBtn = document.querySelector(".modal-close-button")
+const projectBtn1 = document.getElementById("work1");
+const projectBtn2 = document.getElementById("work2");
+const projectBtn3 = document.getElementById("work3");
+const projectBtn4 = document.getElementById("work4");
 
-const openModal = () =>{
-    customModal.classList.remove('hidden');
-}
+//modal
+const openModal=()=>customModal.classList.remove('hidden');
+const closeModal=()=>customModal.classList.add('hidden');
 
-const closeModal = () =>{
-    customModal.classList.add('hidden');
-}
-
-projectBtn.addEventListener("click", openModal);
+//EventListener
 closeBtn.addEventListener("click", closeModal);
+projectBtn1.addEventListener("click", openModal);
+projectBtn2.addEventListener("click", openModal);
+projectBtn3.addEventListener("click", openModal);
+projectBtn4.addEventListener("click", openModal);
+
 
 
 
